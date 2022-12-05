1c92fe2815e4f9cccf3292206ab9502bca0c690b
yovelzig <yovel20031@gmail.com>
2022-12-05 12:54:50 +0200

    first4

--- my_mat.c
+++ my_mat.c
@@ -2,19 +2,39 @@
 #include"my_mat.h"
 void algo(int arr[10][10])
 {
-  for (int k = 0; k < 10; k++)
-  {
-    for (int i = 0; i < 10; i++)
-    {
-      for (int j = 0; j < 10; j++)
-      {
-        if (arr[i][j] < arr[i][k] + arr[k][j])
-        {
-          arr[i][j] = arr[i][k] + arr[k][j];
+      for (int k = 0; k < 10; k++) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i == j)
+                    arr[i][j] = 0;
+                else {
+                    if ((arr[i][k] != -1) && (arr[k][j] != -1)) {
+                        int m = arr[i][k] + arr[k][j];
+                        if (arr[i][j] <= 0)
+                            arr[i][j] = m;
+                        else {
+                            if (arr[i][j] > arr[i][k] + arr[k][j]) {
+                                arr[i][j] = arr[i][k] + arr[k][j];
+                            }
+                        }
+                    }
+                }
+            }
         }
-      }
     }
-  }
+  // for (int k = 0; k < 10; k++)
+  // {
+  //   for (int i = 0; i < 10; i++)
+  //   {
+  //     for (int j = 0; j < 10; j++)
+  //     {
+  //       if (arr[i][j] < arr[i][k] + arr[k][j])
+  //       {
+  //         arr[i][j] = arr[i][k] + arr[k][j];
+  //       }
+  //     }
+  //   }
+  // }
 }
 void get_matrix(int arr[10][10])
 {
