6725cd30838b8591c5a36f0dfc9c3da43788b73a
yovelzig <yovel20031@gmail.com>
2022-12-05 13:18:48 +0200

    first7

--- my_mat.c
+++ my_mat.c
@@ -1,58 +1,65 @@
-#include<stdio.h>
-#include"my_mat.h"
+#include <stdio.h>
+#include "my_mat.h"
 void algo(int arr[10][10])
 {
-      for (int k = 0; k < 10; k++) {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (i == j)
-                    arr[i][j] = 0;
-                else {
-                    if ((arr[i][k] != -1) && (arr[k][j] != -1)) {
-                        int m = arr[i][k] + arr[k][j];
-                        if (arr[i][j] <= 0)
-                            arr[i][j] = m;
-                        else {
-                            if (arr[i][j] > arr[i][k] + arr[k][j]) {
-                                arr[i][j] = arr[i][k] + arr[k][j];
-                            }
-                        }
-                    }
-                }
+  for (int k = 0; k < 10; k++)
+  {
+    for (int i = 0; i < 10; i++)
+    {
+      for (int j = 0; j < 10; j++)
+      {
+        if (i == j)
+          arr[i][j] = 0;
+        else
+        {
+          if ((arr[i][k] != 0) && (arr[k][j] != 0))
+          {
+            int m = arr[i][k] + arr[k][j];
+            if (arr[i][j] <= 0)
+              arr[i][j] = m;
+            else
+            {
+              if (arr[i][j] > arr[i][k] + arr[k][j])
+              {
+                arr[i][j] = arr[i][k] + arr[k][j];
+              }
             }
+          }
         }
+      }
     }
+  }
 }
 void get_matrix(int arr[10][10])
 {
-    for (int i = 0; i < 10; i++)
+  for (int i = 0; i < 10; i++)
+  {
+    for (int j = 0; j < 10; j++)
     {
-        for (int j = 0; j < 10; j++)
-        {
-            scanf("%d",&arr[i][j]);
-        }   
+      scanf("%d", &arr[i][j]);
     }
-    algo(arr);
+  }
+  algo(arr);
 }
 void path_exist(int arr[10][10], int i, int j)
-{    
-    if(arr[i][j]!=0)
-    {
-        printf("True");
-    }
-    else
-    {
-        printf("False");
-    }
+{
+  if (arr[i][j] != 0)
+  {
+    printf("True\n");
+  }
+  else
+  {
+    printf("False\n");
+  }
 }
-void shortest_path( int arr[10][10],int i, int j)
-{  
-    if(arr[i][j]!=0)
-    {
-        printf("%d",arr[i][j]);
-    }
-    else
-    {
-        printf("-1");
-    }
+void shortest_path(int arr[10][10], int i, int j)
+{
+  if (arr[i][j] != 0)
+  {
+    printf("%d\n", arr[i][j]);
+  }
+  else
+  {
+    printf("-1\n");
+  }
 }
